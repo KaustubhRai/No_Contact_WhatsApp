116eec52e59c3dfa7b3a00568c732c6ebb4d02cc
KaustubhRai <raikaustubh12@gmail.com>
2022-02-26 20:06:24 +0530

    edited readme and pushed files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # No_Contact_WhatsApp
-Telegram Bot for sending messages on WhatsApp without saving their numbers
+
+A simple Telegram bot for sending messages on WhatsApp without saving their numbers.
+
+Run BotFather on Telegram and make a bot and get a token, insert it bot.py file 
+
+- run "pip install -r requirements.txt"
+
+- run the bot
+
+Just made it for running locally yet, will deploy it in future.
---
+++ bot.py
@@ -0,0 +1,21 @@
+from telegram import Update
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
+import logging
+updater = Updater(token='2134281003:AAFaeJxedUrRCyEUEminSC9qkqBC5fyiEv0', use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Provide me with the number you wish to contact on WhatsApp, with the country code")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"http://api.whatsapp.com/send?phone="+str(update.message.text))
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+updater.start_polling()
